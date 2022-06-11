5a9927a553f6cf7aa2c2e5cf0f116c9ff10ed15f
kvenus-uj <alexstepan0209@gmail.com>
2022-06-11 20:53:37 +0300

    'v1.3'

--- src/App.js
+++ src/App.js
@@ -538,6 +538,7 @@ function App(props) {
       if(rem.length === 0){
         localStorage.setItem(provider.wallet.publicKey.toString()+curQuest.index+"state", "unstake");
         setcurState('unstake');
+        changeStakingIndex(curQuest.index);
         if(curQuest.index === 1) setCurtime(curQuest.time);
         if(curQuest.index === 2) setCurtime1(curQuest.time);
         if(curQuest.index === 3) setCurtime2(curQuest.time);
